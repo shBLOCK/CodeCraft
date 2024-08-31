455964022cd45abda96e02459cc0cd20f8946ee7
shBLOCK <shblock3@gmail.com>
2024-09-01 07:13:42 +0800

    Remove type checking for coroutine function in auto_async to support more general callables that returns an awaitable

--- python/codecraft/coro/coro.py
+++ python/codecraft/coro/coro.py
@@ -19,9 +19,6 @@ def auto_async[** P, R](coro_func: Callable[P, Coroutine[Any, Any, R]]) -> Calla
     When called from a running event loop, return the coroutine created by `coro_func`.
     When called from a normal (synchronous) context, run `_RUNNER.run(coro_func())` and return the result.
     """
-    if not asyncio.iscoroutinefunction(coro_func):
-        raise TypeError("@auto_async can only be used on coroutine functions")
-
     def inner(*args: P.args, **kwargs: P.kwargs) -> MaybeAwaitable[R]:
         # noinspection PyUnresolvedReferences,PyProtectedMember
         loop: Optional[AbstractEventLoop] = asyncio._get_running_loop()
