f92e989e3da37feb22fc1efa416bb9c34a04f58d
shBLOCK <shblock3@gmail.com>
2024-08-31 17:21:39 +0800

    Use neoforge's `ConfigurationScreen`

--- mod/src/main/kotlin/dev/shblock/codecraft/Config.kt
+++ mod/src/main/kotlin/dev/shblock/codecraft/Config.kt
@@ -5,6 +5,8 @@ import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.config.ModConfig
 import net.neoforged.fml.event.config.ModConfigEvent
+import net.neoforged.neoforge.client.gui.ConfigurationScreen
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory
 import net.neoforged.neoforge.common.ModConfigSpec
 import kotlin.reflect.KProperty
 
@@ -18,7 +20,7 @@ internal object Config {
         val shutdownGracePeriod by BUILDER
             .comment("The maximum amount of time (ms) to wait until a server stops gracefully")
             .worldRestart()
-            .defineInRange("server.shutdownGracePeriod", 1000, 0, Int.MAX_VALUE, Int::class.java)
+            .defineInRange("shutdownGracePeriod", 1000, 0, Int.MAX_VALUE, Int::class.java)
     }
 
     init {
@@ -32,7 +34,10 @@ internal object Config {
 
     internal fun init(modContainer: ModContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, SPEC)
-//        modContainer.registerExtensionPoint // TODO: cloth config configuration screen
+        modContainer.registerExtensionPoint(
+            IConfigScreenFactory::class.java,
+            IConfigScreenFactory { container, screen -> ConfigurationScreen(container, screen) }
+        )
     }
 
     @SubscribeEvent
