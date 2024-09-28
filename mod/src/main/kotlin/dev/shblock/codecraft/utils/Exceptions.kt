db4c837e53a43c9d5aae923d06794982707c2089
shBLOCK <shblock3@gmail.com>
2024-09-28 11:42:03 +0800

    Remove unused exceptions in `Exceptions.kt`

--- mod/src/main/kotlin/dev/shblock/codecraft/utils/Exceptions.kt
+++ mod/src/main/kotlin/dev/shblock/codecraft/utils/Exceptions.kt
@@ -1,11 +1,5 @@
 package dev.shblock.codecraft.utils
 
-//interface NotUnexpected
-
-/**
- * Represents an internal (unexpected) exception.
- */
-class InternalException(message: String? = null, cause: Throwable? = null) : RuntimeException(message, cause)
 
 /**
  * Represents an (expected) exception that is likely the result of a user error.
