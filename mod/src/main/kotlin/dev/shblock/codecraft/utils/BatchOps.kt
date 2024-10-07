8197f27980957742cad7fbec33fbee1a9647b59a
shBLOCK <shblock3@gmail.com>
2024-10-07 22:28:04 +0800

    Add BatchOps.kt

---
+++ mod/src/main/kotlin/dev/shblock/codecraft/utils/BatchOps.kt
@@ -0,0 +1,321 @@
+@file:Suppress("DuplicatedCode", "unused")
+
+package dev.shblock.codecraft.utils
+
+
+//@formatter:off
+inline fun <T> onEach(v1: T, v2: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, v17: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+    v17.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, v17: T, v18: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+    v17.block()
+    v18.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, v17: T, v18: T, v19: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+    v17.block()
+    v18.block()
+    v19.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, v17: T, v18: T, v19: T, v20: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+    v17.block()
+    v18.block()
+    v19.block()
+    v20.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, v17: T, v18: T, v19: T, v20: T, v21: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+    v17.block()
+    v18.block()
+    v19.block()
+    v20.block()
+    v21.block()
+}
+
+inline fun <T> onEach(v1: T, v2: T, v3: T, v4: T, v5: T, v6: T, v7: T, v8: T, v9: T, v10: T, v11: T, v12: T, v13: T, v14: T, v15: T, v16: T, v17: T, v18: T, v19: T, v20: T, v21: T, v22: T, block: T.() -> Unit) {
+    v1.block()
+    v2.block()
+    v3.block()
+    v4.block()
+    v5.block()
+    v6.block()
+    v7.block()
+    v8.block()
+    v9.block()
+    v10.block()
+    v11.block()
+    v12.block()
+    v13.block()
+    v14.block()
+    v15.block()
+    v16.block()
+    v17.block()
+    v18.block()
+    v19.block()
+    v20.block()
+    v21.block()
+    v22.block()
+}
+//@formatter:on
